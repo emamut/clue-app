bfd58ee084432e4834ccc7b0ef2fc03e0a12f47c
Faber Vergara <faber@simian.co>
2023-11-27 12:21:51 -0500

    starts vue development

---
+++ app.js
@@ -0,0 +1,32 @@
+const { createApp, ref } = Vue;
+
+createApp({
+  data() {
+    return {
+      dataArray: {
+        name: [],
+        verduzco: [],
+        mostaza: [],
+        marlene: [],
+        moradillo: [],
+        escarlata: [],
+        blanca: [],
+        candelabro: [],
+        daga: [],
+        tubo: [],
+        revolver: [],
+        soga: [],
+        llave: [],
+        salon: [],
+        billar: [],
+        terraza: [],
+        comedor: [],
+        pasillo: [],
+        cocina: [],
+        biblioteca: [],
+        sala: [],
+        estudio: [],
+      },
+    };
+  },
+}).mount('#app');
